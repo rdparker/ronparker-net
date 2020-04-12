b48733f6ae341524b2d1c6941eae42e633e1d824
Ron Parker <rdparker@gmail.com>
2020-04-12 12:20:15 -0400

    Display dates on posts

--- site/src/templates/blog-post.tsx
+++ site/src/templates/blog-post.tsx
@@ -1,9 +1,12 @@
 /* TODO:  Figure out how to add PropType runtime checking for the page query. */
 import React from 'react'
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { jsx } from 'theme-ui'
 import { graphql } from 'gatsby'
 import Layout from '../components/layout'
 import SEO from '../components/seo'
 import { markdownCodeToPrismHtml, removeMarkdown } from '../utils/title'
+import colors from '../gatsby-plugin-theme-ui/colors'
 
 export const query = graphql`
   query BlogPost($slug: String!) {
@@ -13,6 +16,7 @@ export const query = graphql`
       frontmatter {
         title
         description
+        date(formatString: "MMMM DD, YYYY")
       }
     }
   }
@@ -24,6 +28,7 @@ interface Props {
       frontmatter: {
         title: string
         description?: string
+        date?: string
       }
       excerpt?: string
       html: string
@@ -35,6 +40,9 @@ const BlogPost = ({ data }: Props): JSX.Element => {
   const post = data.markdownRemark
   const title = markdownCodeToPrismHtml(post.frontmatter.title)
   const seoTitle = removeMarkdown(post.frontmatter.title)
+  const date = post.frontmatter.date && (
+    <p style={{ color: `${colors.darken}` }}>{post.frontmatter.date}</p>
+  )
 
   return (
     <Layout>
@@ -45,6 +53,7 @@ const BlogPost = ({ data }: Props): JSX.Element => {
       <div>
         {/* eslint-disable react/no-danger */}
         <h1 dangerouslySetInnerHTML={{ __html: title }} />
+        {date}
         <div dangerouslySetInnerHTML={{ __html: post.html }} />
         {/* eslint-enable react/no-danger */}
       </div>
